31ea71b07f49df05a646684bb540730da89929db
Jerod Gawne <jerodgawne@gmail.com>
2017-09-27 09:51:28 -0400

    add 04_division.py

--- 01_Introduction/04_division.py
+++ 01_Introduction/04_division.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3.6
-"""
-Jerod Gawne, 2017-04-19
+"""Jerod Gawne, 2017-04-19
 
 Division
 
 https://www.hackerrank.com/challenges/python-division/
+
+Editorial
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a / b)
 """
 
 
@@ -12,11 +19,9 @@ def main():
     """
     Main-Logic
     """
-    a = int(input())
-    b = int(input())
-    if b != 0:
-        print(a//b)
-        print(a/b)
+    a, b = int(input()), int(input())
+
+    print((a // b), (a / b), sep='\n')
 
 
 if __name__ == '__main__':
