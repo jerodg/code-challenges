7548723e161fe325d9f3fbc6dc7e6e7c3f740adf
Jerod Gawne <jerodgawne@gmail.com>
2017-10-02 17:44:06 -0400

    add two additional methods

--- 03_Strings/01_swap-case.py
+++ 03_Strings/01_swap-case.py
@@ -6,7 +6,10 @@ Swap Case
 https://www.hackerrank.com/challenges/swap-case/
 
 Editorial:
- -
+ - Use the method string.swapcase(s) to swap lower case letters to upper case letters and vice versa.
+
+import string
+print string.swapcase(raw_input())
 """
 
 
@@ -20,11 +23,35 @@ def swap_case(s):
     return ''.join([l.upper() if l.islower() else l.lower() for l in s])
 
 
+def swap_case2(s):
+    """
+    Reverse Case
+
+    Best Method
+
+    :param s:
+    :return:
+    """
+    return s.swapcase()
+
+
+def swap_case3(s):
+    """
+    Reverse Case
+
+    :param s:
+    :return:
+    """
+    return "".join(map(str.swapcase, s))
+
+
 def main():
     """
     Main/Tests
     """
-    print(swap_case(input()))
+    s = input()
+    print(swap_case(s))
+    print(swap_case2(s))
 
 
 if __name__ == '__main__':
