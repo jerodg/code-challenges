90451ec08690f3c30c0105f54ba3db58c2730c0d
Jerod Gawne <jerodgawne@gmail.com>
2017-10-04 11:26:59 -0400

    add additional methods improved documentation

--- 03_Strings/05_find-a-string.py
+++ 03_Strings/05_find-a-string.py
@@ -32,10 +32,11 @@ print len(match)
 
 
 def count_substring(string, sub_string):
-    """
+    """Jerod Gawne, 2017-10-04
+
     Counts the number of occurences of sub_string in string
 
-    Credit: https://stackoverflow.com/questions/2970520/string-count-with-overlapping-occurrences
+    Attribution: https://stackoverflow.com/a/2970542/4434405
 
     :param string:
     :param sub_string:
@@ -51,10 +52,47 @@ def count_substring(string, sub_string):
 
 
 def count_substring2(string, sub_string):
-    """
+    """Jerod Gawne, 2017-10-04
+
     Counts the number of occurences of sub_string in string
 
-    Credit: https://www.hackerrank.com/rsurana?hr_r=1
+    Attribution: https://stackoverflow.com/a/46311005/4434405
+
+    :param string:
+    :param sub_string:
+    :return:
+    """
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+
+
+def count_substring3(string, sub_string):
+    """Jerod Gawne, 2017-10-04
+
+    Counts the number of occurences of sub_string in string
+
+    Attribution: https://stackoverflow.com/a/40317123/4434405
+
+    :param string:
+    :param sub_string:
+    :return:
+    """
+    count = 0
+    while sub_string in string:
+        count += 1
+        string = string[string.find(sub_string) + 1:]
+    return count
+
+
+def count_substring4(string, sub_string):
+    """Jerod Gawne, 2017-10-04
+
+    Counts the number of occurences of sub_string in string
+
+    Attribution: https://www.hackerrank.com/rsurana?hr_r=1
 
     In a list comprehension, we slide through bigger string by one position at a time with the
     sliding window of length of smaller string. We can compute the sliding count by substracting
@@ -66,18 +104,69 @@ def count_substring2(string, sub_string):
     :param sub_string:
     :return:
     """
-    return sum([1 for i in range(len(string) - len(sub_string) + 1) if string[i:i + len(sub_string)] == sub_string])
+    return sum([1 for _ in range(len(string) - len(sub_string) + 1) if string[_:_ + len(sub_string)] == sub_string])
+
+
+def count_substring5(string, sub_string):
+    """Jerod Gawne, 2017-10-04
+
+    Counts the number of occurences of sub_string in string
+
+    Attribution: https://stackoverflow.com/a/11706065/4434405
+
+    re.escape makes sure the substring doesn't interfere with the regex
+
+    This loads the entire list into memory
+
+    :param string:
+    :param sub_string:
+    :return:
+    """
+    import re
+    return len(re.findall(f'(?={re.escape(sub_string)})', string))
+
+
+def count_substring6(string, sub_string):
+    """Jerod Gawne, 2017-10-04
+
+    Counts the number of occurences of sub_string in string
+
+    Attribution: https://stackoverflow.com/a/11706065/4434405
+
+    re.escape makes sure the substring doesn't interfere with the regex
+
+    alternative to in-memory count_substring5()
+
+    This computes occurances which requires cpu cycles but doesn't hold list in memory
+
+    :param string:
+    :param sub_string:
+    :return:
+    """
+    import re
+    return sum(1 for _ in re.finditer(f'(?={re.escape(sub_string)})', string))
 
 
 def main():
     """
     Main/Tests
+
+    Sample Input
+    ABCDCDC
+    CDC
+
+    Sample Output
+    2
     """
     string = input().strip()
     sub_string = input().strip()
 
     print(count_substring(string, sub_string))
     print(count_substring2(string, sub_string))
+    print(count_substring3(string, sub_string))
+    print(count_substring4(string, sub_string))
+    print(count_substring5(string, sub_string))
+    print(count_substring6(string, sub_string))
 
 
 if __name__ == '__main__':
