8e41365e93d7d114ef3400228759c0efa5961a26
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2017-10-04 09:42:27 -0400

    add 04_mutations.py
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ 03_Strings/04_mutations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.6
+"""Jerod Gawne, 2017-10-04
+
+Mutations
+
+https://www.hackerrank.com/challenges/python-mutations/
+
+Editorial:
+ -
+"""
+
+
+def mutate_string(string, position, character):
+    """
+    Changes string at position to character
+
+    :param string:
+    :param position:
+    :param character:
+    :return:
+    """
+    return string[:position] + character + string[position + 1:]
+
+
+def main():
+    """
+    Main/Tests
+    """
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        import sys
+        import traceback
+
+        print(traceback.print_exception(*sys.exc_info()))
