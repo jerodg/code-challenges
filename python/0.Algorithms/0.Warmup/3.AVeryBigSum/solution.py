d774b5289b57841ca24dbe2b612a9ec52c12eaa5
Jerod Gawne <jerodgawne@gmail.com>
2018-06-07 13:15:27 -0400

    add 3.AVeryBigSum; refactor folder names

---
+++ python/0.Algorithms/0.Warmup/3.AVeryBigSum/solution.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.7
+# coding=utf-8
+"""Jerod Gawne, 2018.06.07
+
+https://www.hackerrank.com/challenges/a-very-big-sum/problem
+"""
+import sys
+import traceback
+
+
+def main():
+    """
+    Main/Tests
+    """
+    _ = input()  # required to pass tests
+    print(sum(map(int, input().split())))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        print(traceback.print_exception(*sys.exc_info()))
