2ad259c33f7353b2147039b8f8b3bb2a71e9f9b7
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2018-07-20 12:06:35 -0400

    add itertools.product
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ python/5.Itertools/0.Product/solution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+# coding=utf-8
+"""Jerod Gawne, 2018.07.20
+
+https://github.com/jerodg/hackerrank
+"""
+import itertools
+import sys
+import traceback
+
+if __name__ == '__main__':
+    try:
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+        print(*itertools.product(a, b))
+    except Exception:
+        print(traceback.print_exception(*sys.exc_info()))
