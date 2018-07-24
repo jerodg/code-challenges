5721f50766be9eb3f02f545c94626c444be9fc48
Jerod Gawne <jerodgawne@gmail.com>
2018-07-24 12:43:26 -0400

    add itertools.combinations_with_replacement

--- python/05.Itertools/3.CombinationsWithReplacement/solution.py
+++ python/05.Itertools/3.CombinationsWithReplacement/solution.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.7
 # coding=utf-8
-"""Jerod Gawne, 2018.07.20
+"""Jerod Gawne, 2018.07.24
 
 https://github.com/jerodg/hackerrank
 """
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     try:
         s, plen = input().split()
         s = sorted(s)  # Sort before so the string is not sorted with each iteration of geneartor below
-        print(*(''.join(_) for _ in itertools.permutations(s, int(plen))), sep='\n')
+        print(*(''.join(_) for _ in itertools.combinations_with_replacement(s, int(plen))), sep='\n')
     except Exception:
         print(traceback.print_exception(*sys.exc_info()))
