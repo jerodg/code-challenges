c660e3bb9d2505f4d9046d6e2f4aef13f425b4b0
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2018-07-24 13:07:11 -0400

    add itertools compress the string (groupby)
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ python/05.Itertools/4.CompressTheString/solution2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.7
+# coding=utf-8
+"""Jerod Gawne, 2018.07.24
+
+https://github.com/jerodg/hackerrank
+"""
+import itertools
+import sys
+import traceback
+
+if __name__ == '__main__':
+    try:
+        for k, g in itertools.groupby(input):
+            print("({}, {})".format(len(list(g)), k), end=" ")
+    except Exception:
+        print(traceback.print_exception(*sys.exc_info()))
