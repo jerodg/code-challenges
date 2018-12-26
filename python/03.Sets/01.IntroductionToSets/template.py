b1429708676985a7ddf18172fd2aea71c03145e5
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2018-12-26 08:54:28 -0800

    add 03.Sets/01.IntroductionToSets
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ python/03.Sets/01.IntroductionToSets/template.py
@@ -0,0 +1,8 @@
+def average(array):
+    # your code goes here
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
--- python/template.py
+++ python/template.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3.7
-# coding=utf-8
-"""Jerod Gawne, 2018.10.02 <https://github.com/jerodg>"""
-import sys
-import traceback
+from sys import exc_info
+from traceback import print_exception
 
 if __name__ == '__main__':
     try:
         pass
     except Exception:
-        print(traceback.print_exception(*sys.exc_info()))
+        print(print_exception(*exc_info()))
