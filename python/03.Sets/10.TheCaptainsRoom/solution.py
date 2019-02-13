042c5f07798503850d507703cf2936f7b8131888
Jerod Gawne <jerodgawne@gmail.com>
2019-02-13 10:24:25 -0500

    add 03.sets/10.thecaptainsroom

--- python/03.Sets/10.TheCaptainsRoom/solution.py
+++ python/03.Sets/10.TheCaptainsRoom/solution.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3.7
-"""03.Sets/09.Mutations: Jerod Gawne, 2019.02.12 <https://github.com/jerodg>"""
+"""The Captains Rooms: Jerod Gawne, 2019.02.13 <https://github.com/jerodg>"""
 from sys import exc_info
 from traceback import print_exception
 from typing import NoReturn
 
 
 def main() -> NoReturn:
-    input()
-    a = set(input().split())
-
-    for _ in range(int(input())):
-        cmd, *args = input().split()
-        getattr(a, cmd)(set(input().split()))
-
-    print(sum(map(int, a)))
+    k, arr = int(input()), list(map(int, input().split()))
+    print(((sum(set(arr)) * k) - (sum(arr))) // (k - 1))
 
 
 if __name__ == '__main__':
---
+++ python/03.Sets/10.TheCaptainsRoom/solution1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.7
+"""The Captains Rooms: Jerod Gawne, 2019.02.13 <https://github.com/jerodg>"""
+from sys import exc_info
+from traceback import print_exception
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    _, s = int(input()), input().split()
+    d = {}
+    for item in s:
+        try:
+            d[item] += 1
+        except KeyError:
+            d[item] = 1
+
+    for k, v in d.items():
+        if v == 1:
+            print(k)
+            break
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as excp:
+        print(print_exception(*exc_info()))
