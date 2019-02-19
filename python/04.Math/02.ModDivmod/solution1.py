e17d6d9198cde59eaf377a12285790f191c85abc
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2019-02-19 09:46:12 -0500

    add 04.math/02.moddivmod
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ python/04.Math/02.ModDivmod/solution1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+"""Mod Divmod: Jerod Gawne, 2019.02.19 <https://github.com/jerodg>"""
+from sys import exc_info
+from traceback import print_exception
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    a, b = int(input()), int(input())
+    print(a // b, a % b, divmod(a, b), sep='\n')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as excp:
+        print(print_exception(*exc_info()))
