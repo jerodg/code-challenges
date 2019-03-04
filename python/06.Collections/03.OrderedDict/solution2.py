568e2f3f5e273185133e5e1d67d52d544260d65f
Jerod Gawne <jerodgawne@gmail.com>
2019-03-04 09:28:27 -0500

    add 06.collections/03.ordereddict

---
+++ python/06.Collections/03.OrderedDict/solution2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.7
+"""Jerod Gawne, 2019.03.04 <https://github.com/jerodg>"""
+import sys
+import traceback
+from collections import OrderedDict
+
+if __name__ == '__main__':
+    try:
+        n = int(input())
+        d = OrderedDict()
+        for i in range(n):
+            k, v = input().rsplit(None, 1)
+            v = int(v)
+            try:
+                d[k] += v
+            except KeyError:
+                d[k] = v
+        [print(k, v) for k, v in d.items()]
+    except Exception:
+        print(traceback.print_exception(*sys.exc_info()))
