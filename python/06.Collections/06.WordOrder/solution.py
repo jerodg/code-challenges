ca2459649d6a2694e7a0f3b06106a13ee4148b52
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2019-03-07 10:10:11 -0500

    add 06.collections/06.wordorder
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

--- python/06.Collections/06.WordOrder/solution.py
+++ python/06.Collections/06.WordOrder/solution.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3.7
-"""Most Common: Jerod Gawne, 2019.03.06 <https://github.com/jerodg/hackerrank>"""
+"""Word Order: Jerod Gawne, 2019.03.07 <https://github.com/jerodg/hackerrank>"""
 import sys
 import traceback
 from collections import Counter
 
 if __name__ == '__main__':
     try:
-        [print(*_) for _ in Counter(sorted(input())).most_common(3)]
+        d = Counter(input() for _ in range(int(input())))
+        print(len(d))
+        print(*d.values())
     except Exception:
         print(traceback.print_exception(*sys.exc_info()))
---
+++ python/06.Collections/06.WordOrder/solution1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.7
+"""Word Order: Jerod Gawne, 2019.03.07 <https://github.com/jerodg/hackerrank>"""
+import sys
+import traceback
+
+if __name__ == '__main__':
+    try:
+        d = {}
+        for i in range(int(input())):
+            # If input not in the dictionary, then add it; else increment the counter
+            key = input()
+            if key not in d.keys():
+                d.update({key: 1})
+                continue
+            d[key] += 1
+
+        print(len(d.keys()))
+        print(*d.values())
+    except Exception:
+        print(traceback.print_exception(*sys.exc_info()))
