e4fd82f28102a6bc32c857821fba49a3eb1becb4
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2020-02-03 10:56:24 -0500

    Add Collections 07.PilingUp
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ python/06.Collections/07.PilingUp/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.8
+"""
+Piling Up: Jerod Gawne, 2020.02.03 <https://github.com/jerodg/hackerrank>
+"""
+from collections import deque
+
+
+def main():
+    for _ in (range(int(input()))):
+        input()  # Discard length of list
+        side_lengths = deque(map(int, input().strip().split()))
+        if max(side_lengths) not in (side_lengths[0], side_lengths[-1]):
+            print('No')
+        else:
+            print('Yes')
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/06.Collections/07.PilingUp/solution1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.8
+"""
+Piling Up: Jerod Gawne, 2020.02.03 <https://github.com/jerodg/hackerrank>
+"""
+
+
+def main():
+    for t in range(int(input())):
+        l = int(input())
+        lst = list(map(int, input().split()))
+        i = 0
+        while i < l - 1 and lst[i] >= lst[i + 1]:
+            i += 1
+
+        while i < l - 1 and lst[i] <= lst[i + 1]:
+            i += 1
+
+        print('Yes' if i == l - 1 else 'No')
+
+
+if __name__ == '__main__':
+    main()
