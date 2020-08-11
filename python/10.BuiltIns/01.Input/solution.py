53b72963fa35313a79c9a02b20aff172eb63b1f2
Jerod Gawne <jerodgawne@gmail.com>
2020-08-11 10:08:29 -0400

    Add 10.Builtins/01.Input

--- python/10.BuiltIns/01.Input/solution.py
+++ python/10.BuiltIns/01.Input/solution.py
@@ -5,7 +5,8 @@ Input Jerod Gawne, 2020.08.06 <https://github.com/jerodg/hackerrank>
 
 
 def main():
-    pass
+    x, k = map(int, input().split())
+    print(k == eval(input()))
 
 
 if __name__ == '__main__':
