634d4cc01c2aaece65237a4d5e19e588454474fd
Jerod Gawne <2856163+jerodg@users.noreply.github.com>
2021-01-22 15:09:36 -0500

    +Add 10.Builtins/05.ginortS; Update template
    Signed-off-by: Jerod Gawne <2856163+jerodg@users.noreply.github.com>

---
+++ python/10.BuiltIns/05.ginortS/solution.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.8
+"""
+<project_name> Jerod Gawne, 2020.02.03 <https://github.com/jerodg/hackerrank>
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
