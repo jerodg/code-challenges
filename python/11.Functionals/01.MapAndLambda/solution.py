e31bba4cfd2f5bbdd716ae0b21db1166e83d8851
Jerod Gawne <jerodgawne@gmail.com>
2021-07-15 10:45:50 -0700

    update

---
+++ python/11.Functionals/01.MapAndLambda/solution.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.9
+"""
+<project_name> Jerod Gawne, 2021.01.22 <https://github.com/jerodg/hackerrank>
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
