25748da34209898a0b3ff017d08c9153a35edd1b
Jerod Gawne <jerodgawne@gmail.com>
2021-09-13 12:51:24 -0700

    Update/Cleanup

--- python/11.Functionals/01.MapAndLambda/template.py
+++ python/11.Functionals/01.MapAndLambda/template.py
@@ -1,6 +1,10 @@
-cube = lambda x: # complete the lambda function
+cube = lambda x: x  # complete the lambda function
+
 
 def fibonacci(n):
+    pass
     # return a list of fibonacci numbers
 
+
 if __name__ == '__main__':
+    pass
