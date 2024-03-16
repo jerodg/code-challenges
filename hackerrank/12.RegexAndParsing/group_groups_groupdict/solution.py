233b303fba0d4592db8cb3f0fc53306fd6a96514
JerodG <2856163+jerodg@users.noreply.github.com>
2024-03-16 12:22:56 -0400

    feat(group_groups_groupdict): add solution
    + solution
    ~ migrate to rye/ruff

---
+++ hackerrank/12.RegexAndParsing/group_groups_groupdict/solution.py
@@ -0,0 +1,22 @@
+"""
+
+Copyright ©2024 Jerod Gawne <https://github.com/jerodg/>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Server Side Public License (SSPL) as
+published by MongoDB, Inc., either version 1 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+SSPL for more details.
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+You should have received a copy of the SSPL along with this program.
+If not, see <https://www.mongodb.com/licensing/server-side-public-license>."""
+import re
+m = re.search(r'([A-Za-z0-9])\1', input())
+res = m.group(1) if m else -1
+print(res)
