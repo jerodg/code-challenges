d18b403cfc0ad93ec42157d5c4909c6c9168d43f
JerodG <2856163+jerodg@users.noreply.github.com>
2024-04-04 10:52:33 -0400

    feat(leet_code): add solutions
    + 27
    + 1614

---
+++ c#/leet_code/1614.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public int MaxDepth(string s) {
+        int maxDepth = 0;
+        int currentDepth = 0;
+        foreach(char c in s) {
+            if(c == '(') {
+                currentDepth++;
+                maxDepth = Math.Max(maxDepth, currentDepth);
+            } else if(c == ')') {
+                currentDepth--;
+            }
+        }
+        return maxDepth;
+    }
+}
---
+++ c#/leet_code/27.cs
@@ -0,0 +1,12 @@
+﻿public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int i = 0;
+        for(int j = 0; j < nums.Length; j++) {
+            if(nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
