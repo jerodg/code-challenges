55d4e00dd7977c80f76f9fa72167b0140f908877
JerodG <2856163+jerodg@users.noreply.github.com>
2024-04-04 10:56:31 -0400

    feat(leet_code) add solution
    + 27
    + 1614

---
+++ php/leet_code/1614.php
@@ -0,0 +1,21 @@
+<?php
+    class Solution {
+
+        /**
+         * @param String $s
+         * @return Integer
+         */
+        function maxDepth($s) {
+            $max = 0;
+            $depth = 0;
+            for ($i = 0, $iMax = strlen($s); $i < $iMax; $i++) {
+                if ($s[$i] === '(') {
+                    $depth++;
+                    $max = max($max, $depth);
+                } elseif ($s[$i] === ')') {
+                    $depth--;
+                }
+            }
+            return $max;
+        }
+    }
---
+++ php/leet_code/27.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Solution
+    {
+
+        /**
+         * @param Integer[] $nums
+         * @param Integer $val
+         * @return Integer
+         */
+        function removeElement(&$nums, $val)
+        {
+            $i = 0;
+            $n = count($nums);
+            for ($j = 0; $j < $n; $j++) {
+                if ($nums[$j] != $val) {
+                    $nums[$i] = $nums[$j];
+                    $i++;
+                }
+            }
+            return $i;
+        }
+    }
