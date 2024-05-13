edd784f459157aff6da2c7013834ace5845d5762
JerodG <2856163+jerodg@users.noreply.github.com>
2024-05-13 15:35:44 -0400

    update

---
+++ dart/leet_code/main.dart
@@ -0,0 +1,34 @@
+void main() {
+  String str = "I’ve listened to Louis’ episode on the Changelog, and it’s my"
+      " understanding that he sees Gleam as to taco nacho";
+  // String subStr = "to";
+  // int cnt = subStr.allMatches(str).length;
+  // print('Found "$subStr", $cnt times.');
+  // countChars(str);
+  // print(findChar(str, "&"));
+  // print(findChar(str, "t"));
+  // print(findChar(str, " "));
+}
+
+// void countChars(String str) {
+//   Map<String, int> charCount = {};
+//
+//   for (int i = 0; i < str.length; i++) {
+//     String char = str[i];
+//     if (charCount.containsKey(char)) {
+//       charCount[char] = (charCount[char] as int) + 1;
+//     } else {
+//       charCount[char] = 1;
+//     }
+//   }
+//   charCount.forEach((key, value) {print('Char: "$key" occurred: $value times');
+//   });
+// }
+
+// bool findChar(String str, String charToFind) {
+//   for (int i = 0; i < str.length; i++) {
+//     if (str[i] == charToFind) {
+//       return true;
+//     }
+//   } return false;
+// }
